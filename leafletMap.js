1d95cf9360ce83a003634ded0468d2bd28eefd64
srldl <siri.uldal@gmail.com>
2016-09-27 13:47:58 +0200

    leaflet draw added

--- leafletMap.js
+++ leafletMap.js
@@ -9,24 +9,35 @@
             //alert("Hello from the " + name );
             console.log(formdata);
 
-        /*    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+            var map = L.map('mapid').setView([78.000, 16.000], 4);
+
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-                '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            id: 'mapbox.streets'
-        }).addTo(mymap); */
+                maxZoom: 18,
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                id: 'mapbox.streets'
+            }).addTo(map);
 
-       var map = L.map('mapid').setView([78.000, 16.000], 4);
+            // Initialise the FeatureGroup to store editable layers
+            var drawnItems = new L.FeatureGroup();
+            map.addLayer(drawnItems);
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-      maxZoom: 18,
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      id: 'mapbox.streets'
-}).addTo(map);
+
+            //Create a draw control
+            var drawControl = new L.Control.Draw({
+                draw: {
+                    position: 'topleft',
+                    polygon: false,
+                    polyline: false,
+                    rectangle: true,
+                    circle: false,
+                    marker: false
+                },
+            edit: { featureGroup: drawnItems, remove:true }
+            });
+
+            map.addControl(drawControl);
 
         }
         return leafletMap;
