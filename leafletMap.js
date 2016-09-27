7b59f24483f8c9500b6f31a52c3e33af6e16d7b2
srldl <siri.uldal@gmail.com>
2016-09-27 13:33:39 +0200

    map of svalbard lib created

--- leafletMap.js
+++ leafletMap.js
@@ -3,9 +3,31 @@
     'use strict';
     function define_leafletMap(){
         var leafletMap = {};
-        var name = "Norwegian Polar Institute";
+        //var name = "Norwegian Polar Institute";
+
         leafletMap.createmap  = function(){
-            alert("Hello from the " + name );
+            //alert("Hello from the " + name );
+            console.log(formdata);
+
+        /*    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            id: 'mapbox.streets'
+        }).addTo(mymap); */
+
+       var map = L.map('mapid').setView([78.000, 16.000], 4);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+      maxZoom: 18,
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      id: 'mapbox.streets'
+}).addTo(map);
+
         }
         return leafletMap;
     }
