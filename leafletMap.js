1f91fd3d0354b30ec47c4cba9ebb4cefd4ef59c3
srldl <siri.uldal@gmail.com>
2016-09-27 09:18:23 +0200

    first test

---
+++ leafletMap.js
@@ -0,0 +1,19 @@
+(function(window){
+    //I recommend this
+    'use strict';
+    function define_leafletMap(){
+        var leafletMap = {};
+        var name = "Norwegian Polar Institute";
+        leafletMap.greet = function(){
+            alert("Hello from the " + name );
+        }
+        return leafletMap;
+    }
+    //define globally if it doesn't already exist
+    if(typeof(leafletMap) === 'undefined'){
+        window.leafletMap = define_leafletMap();
+    }
+    else{
+        console.log("leafletMap already defined.");
+    }
+})(window);
